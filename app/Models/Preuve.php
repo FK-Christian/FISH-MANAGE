4083620faaf738ac36d77a42ce3760ecffaf1053
BJFT <70958312+BJFT-IT@users.noreply.github.com>
2021-03-28 16:00:08 +0100

    mis a jour Helper

--- app/Models/Preuve.php
+++ app/Models/Preuve.php
@@ -2,11 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Preuve extends Model
 {
-    use HasFactory;
     protected $table = 'preuves';
 }
--- app/Models/Vague.php
+++ app/Models/Vague.php
@@ -2,11 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Vague extends Model {
 
-    use HasFactory;
     protected $table = 'vagues';
 }
