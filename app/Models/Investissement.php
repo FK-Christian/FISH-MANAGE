2c4e846d2cfd9134dc18eab3bdab10863e6cef1f
BJFT <70958312+BJFT-IT@users.noreply.github.com>
2021-03-28 17:25:13 +0100

    mis a jour Helper

--- app/Models/Investissement.php
+++ app/Models/Investissement.php
@@ -4,7 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Investissement extends Model
-{
-protected $table = 'investissements';
+class Investissement extends Model {
+
+    protected $table = 'investissements';
+    
+        protected $fillable = [
+            'balance',
+            'agent',
+            'start_date',
+            'last_modification',
+            'max_to_give'
+    ];
+
 }
