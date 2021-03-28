9cb0ab0852e3dc99ae1f7383e58470372193bc8e
BJFT <70958312+BJFT-IT@users.noreply.github.com>
2021-03-28 17:23:29 +0100

    mis a jour Helper

--- app/Models/Investissement.php
+++ app/Models/Investissement.php
@@ -2,10 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Investissement extends Model
 {
-    use HasFactory;
+protected $table = 'investissements';
 }
