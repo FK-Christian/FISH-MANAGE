c3ade80bbb512a5da3b28f3d03fb509c6fc5d653
BJFT <70958312+BJFT-IT@users.noreply.github.com>
2021-03-29 19:04:18 +0100

    mis a jour HasFactory

--- app/Http/Controllers/ApiWebhookController.php
+++ app/Http/Controllers/ApiWebhookController.php
@@ -277,7 +277,7 @@ class ApiWebhookController extends \crocodicstudio\crudbooster\controllers\ApiCo
                         } else {
                             $data = $this->get_user_and_dataSaved();
                             $nbre = $data['nbre_photo'];
-                            $position_suivante = ($nbre + 1);
+                            $position_suivante = 0; //($nbre + 1);
                             for ($i = 1; $i <= $nbre; $i++) {
                                 $next_to_save = "photo_$i";
                                 if (!isset($data[$next_to_save])) {
@@ -286,7 +286,7 @@ class ApiWebhookController extends \crocodicstudio\crudbooster\controllers\ApiCo
                                     break;
                                 }
                             }
-                            if ($position_suivante < $nbre) {
+                            if ($position_suivante > 0 && $position_suivante <= $nbre) {
                                 $this->navigation->customer_next_step = $this->getStepCode($service . "_PHOTO") . "_$position_suivante";
                             } else {
                                 $this->navigation->customer_next_step = $this->navigation->customer_current_step;
