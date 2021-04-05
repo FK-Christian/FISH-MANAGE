88edeff2ab4fbdaf0a3f811de5d5c2cd541eb98f
BJFT <70958312+BJFT-IT@users.noreply.github.com>
2021-04-05 15:51:34 +0100

    mis a jou

--- app/Models/Bac_and_vangue.php
+++ app/Models/Bac_and_vangue.php
@@ -4,4 +4,10 @@ namespace App\Models;
 
 class Bac_and_vangue extends GeneralModel {
     protected $table = 'bac_and_vangues';
+    protected $fillable = [
+        'id',
+        'bac',
+        'vague',
+        'nbre'
+    ];
 }
